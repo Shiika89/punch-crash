4d9b360e8c974255ea235b54e9e456b1e9da8457
RYUKI <ryukiarashi@icloud.com>
2021-06-25 11:22:33 +0900

    Revert "下から迫る壁のバグを修正しました。"
    This reverts commit 98952ca8068c13f0101489deacc28b4229c083e4.

--- Assets/Scripts/DeathZoneController.cs
+++ Assets/Scripts/DeathZoneController.cs
@@ -5,14 +5,13 @@ using Photon.Pun;
 /// <summary>
 /// 迫りくる壁を制御するコンポーネント
 /// </summary>
-[RequireComponent(typeof(Rigidbody2D))]
 public class DeathZoneController : MonoBehaviour
 {
     /// <summary>移動速度</summary>
     [SerializeField] float m_moveSpeed = 1f;
     /// <summary>移動方向</summary>
     [SerializeField] Vector2 m_moveDirection = Vector2.up;
-    Rigidbody2D m_rb = default;
+    PhotonView m_view = default;
     /// <summary>時間</summary>
     public float m_countdown = 1f;
 
@@ -22,14 +21,14 @@ public class DeathZoneController : MonoBehaviour
 
     void Start()
     {
-        m_rb = GetComponent<Rigidbody2D>();
+        m_view = GetComponent<PhotonView>();
         m_gm = FindObjectOfType<GameManager>();
     }
 
     void Update()
     {
         // オーナー側で移動させる
-        if (m_gm.InGame)
+        if (m_view && m_gm.InGame)
         {
             CountDown();
         }
@@ -45,7 +44,7 @@ public class DeathZoneController : MonoBehaviour
                 isFlag = true;
                 Debug.Log("時間になった");
             }
-            m_rb.MovePosition((Vector2)this.transform.position + m_moveSpeed * m_moveDirection * Time.deltaTime);
+            transform.Translate(m_moveSpeed * m_moveDirection * Time.deltaTime);
 
             m_countdown = 0;
         }
