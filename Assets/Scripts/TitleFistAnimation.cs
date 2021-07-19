19df2ab9d2aa2192660a73b395e144274cde2e76
shiika89 <kazuki_plant@yahoo.co.jp>
2021-07-19 16:21:45 +0900

    TimeLineとAnimatorを使ってプレイヤーのAnimationを追加

---
+++ Assets/Scripts/TitleFistAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleFistAnimation : MonoBehaviour
+{
+    Animator m_anim;
+    [SerializeField] float m_waitTime = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+
+        StartCoroutine("Fist");
+    }
+
+    private IEnumerator Fist()
+    {
+        yield return new WaitForSeconds(m_waitTime);
+
+        m_anim.enabled = true;
+    }
+}
